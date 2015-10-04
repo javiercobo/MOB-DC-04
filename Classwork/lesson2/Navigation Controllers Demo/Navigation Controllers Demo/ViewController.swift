f713c7f956c4fa192f760a144fbe928f0549bf11
Javi Cobo <yocobo@gmail.com>
2015-10-04 11:32:55 -0400

    Classroom play
    Created colors app and my profile

---
+++ Classwork/lesson2/Navigation Controllers Demo/Navigation Controllers Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Navigation Controllers Demo
+//
+//  Created by Cobo, Javier on 9/30/15.
+//  Copyright (c) 2015 Cobo, Javier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
