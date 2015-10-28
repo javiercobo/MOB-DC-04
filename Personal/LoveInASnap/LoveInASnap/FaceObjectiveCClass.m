0bd6b4a00a7e54012b6de526c01efd75b601014c
Javi Cobo <yocobo@gmail.com>
2015-10-28 14:22:48 -0400

    Picture to Text Project

---
+++ Personal/LoveInASnap/LoveInASnap/FaceObjectiveCClass.m
@@ -0,0 +1,13 @@
+//
+//  FaceObjectiveCClass.m
+//  LoveInASnap
+//
+//  Created by Cobo, Javier on 10/28/15.
+//  Copyright © 2015 Lyndsey Scott. All rights reserved.
+//
+
+#import "FaceObjectiveCClass.h"
+
+@implementation FaceObjectiveCClass
+
+@end
