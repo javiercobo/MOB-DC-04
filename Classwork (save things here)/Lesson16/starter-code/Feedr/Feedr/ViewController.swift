2c85fa5b89593d2b29503c7fd56ca826ccf23e08
Javi Cobo <yocobo@gmail.com>
2015-11-25 18:29:27 -0500

    Nov 23 class updates

--- Classwork (save things here)/Lesson16/starter-code/Feedr/Feedr/ViewController.swift
+++ Classwork (save things here)/Lesson16/starter-code/Feedr/Feedr/ViewController.swift
@@ -7,27 +7,13 @@
 //
 
 import UIKit
-import SwiftyJSON
-
 
 class ViewController: UITableViewController {
     var tempData = ["Ena's tail is apperantly OK", "MOB Class is awesome", "Pigs sighted flying"]
     
-    var redditData: JSON?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let url = NSURL(string: "http://www.reddit.com/.json"){
-            let task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
-
-          
-                self.redditData = JSON(data: data!)
-                print(self.redditData)
-                self.tableView.reloadData()
-            })
-            task.resume()
-        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -36,35 +22,22 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if self.redditData != nil {
-        return self.redditData!["data"]["children"].count
-        }
-        return 0
-
+        return self.tempData.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell!
         if cell == nil  {
-            cell = UITableViewCell(style: .Subtitle, reuseIdentifier: "cell")
+            cell = UITableViewCell(style: .Default, reuseIdentifier: "cell")
         }
-        cell.textLabel?.text = self.redditData!["data"]["children"][indexPath.row]["data"]["title"].string
-    let numberOfUps = self.redditData!["data"]["children"][indexPath.row]["data"]["ups"].int
-        cell.detailTextLabel?.text = "Number of ups: \(numberOfUps!)"
-
+        cell.textLabel?.text = self.tempData[indexPath.row]
         return cell
     }
     
     //on click
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        if self.redditData != nil {
-                    let permalink = self.redditData!["data"]["children"][indexPath.row]["data"]["permalink"].string
-            let url = NSURL(string: "http://www.reddit.com" + permalink!)
-             performSegueWithIdentifier("web", sender: NSURLRequest(URL: url!))
-        } else {
-            let url = NSURL(string: "http://tedi.io")
-            performSegueWithIdentifier("web", sender: NSURLRequest(URL: url!))
-        }
+        let url = NSURL(string: "http://tedi.io")
+        performSegueWithIdentifier("web", sender: NSURLRequest(URL: url!))
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
